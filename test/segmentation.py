e3be2155a3c1b49b0b59b4d516c7ce1a022866b3
palkeo <contact@palkeo.com>
2015-05-04 17:12:38 +0200

    Segmentation test.

---
+++ test/segmentation.py
@@ -0,0 +1,23 @@
+import re
+
+from lm import LM
+
+RE_WORD = re.compile(r"\w+", re.IGNORECASE)
+
+def test_basic_segmentation():
+    l = LM(5)
+
+    doc = open('fixtures/btree.txt').read()
+    sentences = doc.split('.')
+    sentences = list(filter(None, map(lambda p: RE_WORD.findall(p), sentences)))
+
+    for sentence in sentences:
+        l.add_sentence(sentence)
+
+    for sentence in sentences:
+        print(sentence)
+        print(l.segment(sentence))
+        print()
+
+if __name__ == '__main__':
+    test_basic_segmentation()
