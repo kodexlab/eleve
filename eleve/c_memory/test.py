0e3fc0b9c107f51bc0682ec053d69b146e84a92c
palkeo <contact@palkeo.com>
2015-06-05 21:09:18 +0200

    First version of a working C trie.

---
+++ eleve/c_memory/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from eleve_trie import Trie
+from eleve.memory import MemoryStorage
+import random
+random.seed('palkeo')
+
+t = Trie()
+t2 = MemoryStorage(4)
+
+for docid in range(1000):
+    for ngram in range(4000):
+        n = [int(random.expovariate(1e-2)) for _ in range(4)]
+        t.add_ngram(n, docid, 1)
--- eleve/c_memory/trie.cpp
+++ eleve/c_memory/trie.cpp
@@ -2,13 +2,18 @@
 #include <memory>
 #include <assert.h>
 #include <map>
+#include <iostream>
+
+#include <boost/python.hpp>
+#include <boost/python/stl_iterator.hpp>
 
 const size_t BLOCK_MAX_SIZE = 100;
 
-typedef unsigned int ID;
-typedef unsigned int COUNT;
+typedef uint32_t ID;
+typedef uint32_t COUNT;
 typedef std::vector<ID>::const_iterator shingle_const_iterator;
 
+class Block;
 class LeafBlock;
 class ListBlock;
 struct TokenBlock;
@@ -21,6 +26,20 @@ struct ShingleInfo
     ShingleInfo(ID d, COUNT c): docid(d), count(c) {};
 };
 
+class BlockIterator
+{
+    public:
+    virtual void next() = 0;
+    virtual std::pair<ID, Block*> get() = 0;
+};
+
+class EmptyBlockIterator : public BlockIterator
+{
+    public:
+    void next() {};
+    std::pair<ID, Block*> get() { return std::pair<ID, Block*>(NULL, nullptr); };
+};
+
 struct Block
 {
     virtual void add_shingle(shingle_const_iterator shingle_it, shingle_const_iterator shingle_end, ShingleInfo& info) = 0;
@@ -28,17 +47,10 @@ struct Block
     virtual size_t size() = 0;
 
     virtual Block* block_for(shingle_const_iterator shingle_it, shingle_const_iterator shingle_end) = 0;
-    virtual ListBlock::iterator begin_childs() = 0;
-    virtual ListBlock::iterator end_childs() = 0;
-    /*virtual LeafBlock::iterator begin_leafs() = 0;
-    virtual LeafBlock::iterator end_leafs() = 0;
-    virtual ShingleInfo::iterator begin_postings() = 0;
-    virtual ShingleInfo::iterator end_postings() = 0;*/
+    virtual std::unique_ptr<BlockIterator> begin_childs() = 0;
 
-    virtual COUNT count();
-    virtual size_t doc_count(); // std::map or hyperloglog
-    virtual std::map<ID, COUNT> postings();
-    virtual float entropy();
+    virtual COUNT count() = 0;
+    //virtual std::map<ID, COUNT> postings();
 };
 
 struct TokenBlock
@@ -62,13 +74,55 @@ class IndexBlock: public Block
 
     IndexBlock() {};
 
+    class IndexBlockIterator: public BlockIterator
+    {
+        private:
+        std::unique_ptr<BlockIterator> current_child_iterator;
+        std::vector<TokenBlock>::iterator current_child;
+        std::vector<TokenBlock>::iterator childs_end;
+
+        public:
+
+        IndexBlockIterator(IndexBlock* b)
+        {
+            childs_end = b->data.end();
+            current_child = b->data.begin();
+            current_child_iterator = current_child->block->begin_childs();
+        }
+
+        IndexBlockIterator(const IndexBlockIterator&) = delete;
+
+        void next()
+        {
+            current_child_iterator->next();
+            if(current_child_iterator->get().second == nullptr && current_child != childs_end)
+            {
+                current_child++;
+                if(current_child != childs_end)
+                {
+                    current_child_iterator = current_child->block->begin_childs();
+                }
+            }
+        };
+
+        std::pair<ID, Block*> get()
+        {
+            return current_child_iterator->get();
+        };
+    };
+
     public:
 
     IndexBlock(TokenBlock& b1, std::unique_ptr<Block>& b2)
     {
         last = std::move(b2);
-        data.push_back(b1);
+        data.push_back(std::move(b1));
     }
+
+    std::unique_ptr<BlockIterator> begin_childs()
+    {
+        return std::unique_ptr<IndexBlockIterator>(new IndexBlockIterator(this));
+    };
 
     TokenBlock split()
     {
@@ -78,18 +132,24 @@ class IndexBlock: public Block
         auto other = std::unique_ptr<IndexBlock>(new IndexBlock());
         other->last = std::move(last);
         last = std::move(data[new_size].block);
-        other->data.assign(data.begin() + new_size + 1, data.end());
-        data.resize(new_size);
+        for(auto it = data.begin() + new_size + 1; it != data.end(); ++it)
+        {
+            other->data.push_back(std::move(*it));
+        }
+        data.erase(data.begin() + new_size, data.end());
         return TokenBlock(token, std::move(other));
     }
 
     void add_shingle(shingle_const_iterator shingle_it, shingle_const_iterator shingle_end, ShingleInfo& info)
     {
+        assert(shingle_it != shingle_end);
+
         auto it = data.begin();
-        while(it < data.end() && *shingle_it > it->token)
+        while(it != data.end() && *shingle_it > it->token)
         {
             ++it;
         }
+
         if(it == data.end())
         {
             last->add_shingle(shingle_it, shingle_end, info);
@@ -99,13 +159,13 @@ class IndexBlock: public Block
             it->block->add_shingle(shingle_it, shingle_end, info);
         }
 
-        if(it->block->size() > BLOCK_MAX_SIZE)
+        if((it == data.end() ? last->size() : it->block->size()) > BLOCK_MAX_SIZE)
         {
             if(it == data.end())
             {
                 auto tb = last->split();
                 auto tb2 = TokenBlock(tb.token, std::move(last));
-                data.push_back(tb);
+                data.push_back(std::move(tb2));
                 last = std::move(tb.block);
             }
             else
@@ -115,19 +175,19 @@ class IndexBlock: public Block
                 it->token = tb.token;
                 ++it;
                 auto tb2 = TokenBlock(left_token, std::move(tb.block));
-                data.insert(it, tb2);
+                data.insert(it, std::move(tb2));
             }
         }
     }
 
     Block* block_for(shingle_const_iterator shingle_it, shingle_const_iterator shingle_end)
     {
-        auto it = data.cbegin();
-        while(it < data.cend() && *shingle_it > it->token)
+        auto it = data.begin();
+        while(it != data.end() && *shingle_it > it->token)
         {
             ++it;
         }
-        if(it == data.cend())
+        if(it == data.end())
         {
             return last->block_for(shingle_it, shingle_end);
         }
@@ -139,6 +199,16 @@ class IndexBlock: public Block
         return data.size() + 1;
     };
 
+    COUNT count()
+    {
+        COUNT c = 0;
+        for(auto it = data.begin(); it != data.end(); ++it)
+        {
+            c += it->block->count();
+        }
+        c += last->count();
+        return c;
+    };
 };
 
 class LeafBlock: public Block
@@ -170,7 +240,7 @@ class LeafBlock: public Block
         assert(shingle_it == shingle_end);
 
         auto it = data.begin();
-        while(it->docid < info.docid && it < data.end())
+        while(it != data.end() && it->docid < info.docid)
         {
             ++it;
         }
@@ -189,14 +259,32 @@ class LeafBlock: public Block
         size_t new_size = data.size() / 2;
         auto token = data[new_size].docid;
         auto other = std::unique_ptr<LeafBlock>(new LeafBlock());
-        other->data.assign(data.begin() + new_size, data.end());
-        data.resize(new_size);
+        for(auto it = data.begin() + new_size + 1; it != data.end(); ++it)
+        {
+            other->data.push_back(std::move(*it));
+        }
+        data.erase(data.begin() + new_size, data.end());
         return TokenBlock(token, std::move(other));
     };
 
     size_t size()
     {
         return data.size();
+    };
+
+    std::unique_ptr<BlockIterator> begin_childs()
+    {
+        return std::unique_ptr<EmptyBlockIterator>(new EmptyBlockIterator());
+    };
+
+    COUNT count()
+    {
+        COUNT c = 0;
+        for(auto it = data.begin(); it != data.end(); ++it)
+        {
+            c += it->count;
+        }
+        return c;
     };
 };
 
@@ -210,19 +298,55 @@ class ListBlock: public Block
     private:
     std::vector<TokenBlock> data;
 
-    ListBlock() {};
+    class ListBlockIterator: public BlockIterator
+    {
+        private:
+        std::vector<TokenBlock>::iterator current_child;
+        std::vector<TokenBlock>::iterator childs_end;
+
+        public:
+
+        ListBlockIterator(ListBlock* b)
+        {
+            childs_end = b->data.end();
+            current_child = b->data.begin();
+        }
+
+        void next()
+        {
+            if(current_child != childs_end)
+            {
+                ++current_child;
+            }
+        };
+
+        std::pair<ID, Block*> get()
+        {
+            return (current_child == childs_end)
+                ? std::pair<ID, Block*>(NULL, nullptr)
+                : std::pair<ID, Block*>(current_child->token, current_child->block.get());
+        };
+    };
+
 
     public:
+
+    ListBlock() {};
 
     ListBlock(shingle_const_iterator shingle_it, shingle_const_iterator shingle_end, ShingleInfo& info)
     {
         auto token = *shingle_it;
+        ++shingle_it;
 
         std::unique_ptr<Block> block;
         if(shingle_end == shingle_it)
+        {
             block = std::unique_ptr<Block>(new LeafBlock(info));
+        }
         else
-            block = std::unique_ptr<Block>(new ListBlock(++shingle_it, shingle_end, info));
+        {
+            block = std::unique_ptr<Block>(new ListBlock(shingle_it, shingle_end, info));
+        }
 
         data.push_back(TokenBlock(token, std::move(block)));
     }
@@ -232,12 +356,12 @@ class ListBlock: public Block
         if(shingle_it == shingle_end)
             return this;
 
-        auto it = data.cbegin();
-        while(it < data.cend() && it->token != *shingle_it)
+        auto it = data.begin();
+        while(it != data.end() && it->token != *shingle_it)
         {
             ++it;
         }
-        if(it == data.cend())
+        if(it == data.end())
             return nullptr;
 
         return (it->block)->block_for(++shingle_it, shingle_end);
@@ -247,13 +371,17 @@ class ListBlock: public Block
     {
         assert(shingle_it != shingle_end);
 
+        auto token = *shingle_it;
+
+        //auto it = std::lower_bound(data.begin(), data.end(), token);
+        // it->token >= token
         auto it = data.begin();
-        while(it->token < *shingle_it && it < data.end())
+        while(it != data.end() && it->token < token)
         {
             ++it;
         }
 
-        if(it->token == *shingle_it)
+        if(it != data.end() && it->token == token)
         {
             // the token exists, add it recursively
             it->block->add_shingle(++shingle_it, shingle_end, info);
@@ -261,8 +389,17 @@ class ListBlock: public Block
         else
         {
             // let's create the token
-            auto new_block = std::unique_ptr<Block>(new ListBlock(++shingle_it, shingle_end, info));
-            it = data.insert(it, TokenBlock(*shingle_it, std::move(new_block)));
+            ++shingle_it;
+            std::unique_ptr<Block> new_block;
+            if(shingle_it == shingle_end)
+            {
+                new_block = std::unique_ptr<Block>(new LeafBlock(info));
+            }
+            else
+            {
+                new_block = std::unique_ptr<Block>(new ListBlock(shingle_it, shingle_end, info));
+            }
+            it = data.insert(it, TokenBlock(token, std::move(new_block)));
         }
 
         if(it->block->size() > BLOCK_MAX_SIZE)
@@ -286,20 +423,89 @@ class ListBlock: public Block
         size_t new_size = data.size() / 2;
         auto token = data[new_size].token;
         auto other = std::unique_ptr<ListBlock>(new ListBlock());
-        other->data.assign(data.begin() + new_size, data.end());
-        data.resize(new_size);
+        for(auto it = data.begin() + new_size + 1; it != data.end(); ++it)
+        {
+            other->data.push_back(std::move(*it));
+        }
+        data.erase(data.begin() + new_size, data.end());
         return TokenBlock(token, std::move(other));
+    };
+
+    std::unique_ptr<BlockIterator> begin_childs()
+    {
+        return std::unique_ptr<ListBlockIterator>(new ListBlockIterator(this));
+    };
+
+    COUNT count()
+    {
+        COUNT c = 0;
+        for(auto it = data.begin(); it != data.end(); ++it)
+        {
+            c += it->block->count();
+        }
+        return c;
     };
 };
 
+namespace py = boost::python;
+
 class Trie
 {
+    protected:
+
+    std::unique_ptr<Block> root;
+
+    void add_shingle(shingle_const_iterator shingle_it, shingle_const_iterator shingle_end, ShingleInfo& info)
+    {
+        root->add_shingle(shingle_it, shingle_end, info);
+
+        if(root->size() > BLOCK_MAX_SIZE)
+        {
+            // tb is the right part of the splitted block + the token in the middle
+            // tb2 is the token in the middle + the left part.
+            auto tb = root->split();
+            auto tb2 = TokenBlock(tb.token, std::move(root));
+            auto last = std::move(tb.block);
+            root = std::unique_ptr<Block>(new IndexBlock(tb2, last));
+        }
+    };
+
+    public:
+
     Trie()
     {
-    }
+        root = std::unique_ptr<Block>(new ListBlock());
+    };
 
-    ~Trie()
+    void add_ngram(py::list ngram, ID docid, COUNT freq)
     {
-    }
+        std::vector<ID> shingle{py::stl_input_iterator<ID>(ngram),
+                                py::stl_input_iterator<ID>()};
 
+        ShingleInfo info(docid, freq);
+
+        add_shingle(shingle.begin(), shingle.end(), info);
+    };
+
+    COUNT query_count(py::list ngram)
+    {
+        std::vector<ID> shingle{py::stl_input_iterator<ID>(ngram),
+                                py::stl_input_iterator<ID>()};
+
+        Block* b = root->block_for(shingle.cbegin(), shingle.cend());
+        if(! b)
+        {
+            return 0;
+        }
+        return b->count();
+    }
 };
+
+BOOST_PYTHON_MODULE(eleve_trie)
+{
+    using namespace boost::python;
+    class_<Trie, boost::noncopyable>("Trie")
+        .def("add_ngram", &Trie::add_ngram)
+        .def("query_count", &Trie::query_count)
+    ;
+}
