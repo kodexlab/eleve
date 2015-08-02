7f9d213c231713eab47792628207a26802e98e93
palkeo <contact@palkeo.com>
2015-08-03 01:28:23 +0200

    Refactor.

--- eleve/__init__.py
+++ eleve/__init__.py
@@ -1,5 +1,1 @@
-from .api import Eleve
-
-__all__ = ['Eleve']
-
 __version__ = '0.2'
--- eleve/segment.py
+++ eleve/segment.py
@@ -1,22 +1,12 @@
 import logging
 
-from storage import Storage
-
 logger = logging.getLogger(__name__)
 
-class Eleve:
-    def __init__(self, order, path, storage_class=Storage, *args, **kwargs):
+class Segmenter:
+    def __init__(self, storage, order):
+        assert hasattr(storage, 'query_autonomy'), "The storage object should have a query_autonomy method."
+        self.storage = storage
         self.order = order
-        self.storage = storage_class(order, path, *args, **kwargs)
-
-    # SENTENCE LEVEL
-
-    def clear(self):
-        self.storage.clear()
-        return self
-
-    def add_sentence(self, sentence, freq=1):
-        self.storage.add_sentence(sentence, freq)
 
     def segment(self, sentence): 
         if len(sentence) > 1000:
@@ -25,7 +15,7 @@ class Eleve:
         sentence = ['^'] + sentence + ['$']
 
         # dynamic programming to segment the sentence
-       
+        
         best_segmentation = [[]]*(len(sentence) + 1)
         best_score = [0] + [float('-inf')]*len(sentence)
 
@@ -37,7 +27,7 @@ class Eleve:
             for j in range(1, self.order + 1):
                 if i - j < 0:
                     break
-                a = self.query_autonomy(sentence[i-j:i])
+                a = self.storage.query_autonomy(sentence[i-j:i])
                 if a != a:
                     a = -100.
                 score = best_score[i-j] + a * j
@@ -53,17 +43,3 @@ class Eleve:
         best_segmentation = list(filter(None, best_segmentation))
 
         return best_segmentation
-
-    # NGRAM LEVEL
-
-    def query_autonomy(self, ngram):
-        return self.storage.query_autonomy(ngram)
-
-    def query_ev(self, ngram):
-        return self.storage.query_ev(ngram)
-
-    def query_count(self, ngram):
-        return self.storage.query_count(ngram)
-
-    def query_entropy(self, ngram):
-        return self.storage.query_entropy(ngram)
