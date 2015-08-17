0d3e0cf00f542fda4240477dd3aa1089b2a7e9f2
palkeo <contact@palkeo.com>
2015-08-18 01:59:33 +0200

    modified setup.py that works but doesn't compile...
    Compiling seems... quite hard to do...

--- eleve/__init__.py
+++ eleve/__init__.py
@@ -1,4 +1,3 @@
-__version__ = '1.0'
 __all__ = ["MemoryStorage", "LeveldbStorage", "Segmenter",
            "CMemoryStorage", "PyMemoryStorage", "CLeveldbStorage", "PyLeveldbStorage"]
 
--- setup.py
+++ setup.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup, find_packages
-from eleve import __version__
 from distutils.core import setup
-from distutils.extension import Extension
+from distutils.command.install import install
 
-#from Cython.Distutils import build_ext
-#from Cython.Build import cythonize
+"""
+class my_install(install):
+    def run(self):
+        install.run(self)
 
+        os.chdir('eleve/c_memory')
+        os.system('cmake . && make')
+        os.chdir('../c_leveldb')
+        os.system('cmake . && make')
+"""
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 readme = open(os.path.join(cwd, 'README.md')).read()
 
 setup(
     name='eleve',
-    version=__version__,
+    version=0.1,
     description="Extraction de LExique par Variation d'Entropie - Lexicon extraction based on the variation of entropy",
     long_description=readme,
     author='Pierre Magistry',
     author_email='pierre@magistry.fr',
     url='http://kodexlab.com/eleve/',
-#    install_requires=['reliure','cython'],
-#    ext_modules=cythonize(['eleve/DTrie.pyx']),
-    packages=['eleve'] + ['eleve.%s' % submod for submod in find_packages('eleve')],
+    packages=['eleve'],
     classifiers=[
         "Programming Language :: Python",
-#        "Programming Language :: Cython", 
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Natural Language :: French",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
         "Topic :: Scientific/Engineering",
     ],
+    #cmdclass={'install': my_install},
 )
 
