32b079bcfa03f7b3a06bffd5e959705d3605eeed
palkeo <contact@palkeo.com>
2015-08-17 19:53:17 -0500

    More fixes for Ubuntu...

--- eleve/__init__.py
+++ eleve/__init__.py
@@ -1,16 +1,16 @@
 __all__ = ["MemoryStorage", "LeveldbStorage", "Segmenter",
            "CMemoryStorage", "PyMemoryStorage", "CLeveldbStorage", "PyLeveldbStorage"]
 
-from segment import Segmenter
+from eleve.segment import Segmenter
 
-from memory_storage import MemoryStorage as PyMemoryStorage
-from leveldb_storage import LeveldbStorage as PyLeveldbStorage
+from eleve.memory_storage import MemoryStorage as PyMemoryStorage
+from eleve.leveldb_storage import LeveldbStorage as PyLeveldbStorage
 
 LeveldbStorage = PyLeveldbStorage
 
 import warnings
 try:
-    from c_memory.cmemory import MemoryStorage as CMemoryStorage
+    from eleve.c_memory.cmemory import MemoryStorage as CMemoryStorage
 except ImportError:
     warnings.warn("Unable to import the C++ memory backend. Eleve will be slower and consume more memory.")
     CMemoryStorage = None
@@ -18,7 +18,7 @@ except ImportError:
 MemoryStorage = CMemoryStorage or PyMemoryStorage
 
 try:
-    from c_leveldb.cleveldb import LeveldbStorage as CLeveldbStorage
+    from eleve.c_leveldb.cleveldb import LeveldbStorage as CLeveldbStorage
 except ImportError:
     warnings.warn("Unable to import the C++ leveldb backend. Eleve will be slower.")
     CLeveldbStorage = None
