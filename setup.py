7902953308c075924a1d06467f5790bf26320da9
Emmanuel N <enavarro222@gmail.com>
2015-10-30 15:54:31 +0100

    Change version to 15.10

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ readme = open(os.path.join(cwd, 'README.rst')).read()
 
 setup(
     name='eleve',
-    version='15.09.r0',
+    version='15.10.r0',
     description="Extraction de LExique par Variation d'Entropie - Lexicon extraction based on the variation of entropy",
     long_description=readme,
     author='KodexLab, Pierre Magistry, Korantin Auguste, Emmanuel Navarro',
