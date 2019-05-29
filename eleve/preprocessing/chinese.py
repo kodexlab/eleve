6265ddfb47b5f6583349c01a420a1a3e72c5ad1d
pierre <pierre@magistry.fr>
2019-05-29 16:33:32 +0200

    segmentation avec preproc par category unicode

--- eleve/preprocessing/chinese.py
+++ eleve/preprocessing/chinese.py
@@ -1,6 +1,7 @@
 from typing import List
 import unicodedata as ud
 
+from eleve import Segmenter
 
 def normalize(s: str) -> str:
     return ud.normalize("NFC", s)
@@ -11,6 +12,8 @@ def tokenize_by_unicode_category(s: str) -> List[str]:
     prev_cat = None
     for c in s:
         cat = ud.category(c)
+        if cat == "Ll" or cat == "Lu":
+            cat = "latin"
         if cat == prev_cat:
             buf[-1] = buf[-1] + c
         else:
@@ -27,3 +30,26 @@ def filter_cjk(toks: List[str]) -> List[str]:
                 pass
         except:
             pass
+
+def add_bies(s: str) -> str:
+    if len(s) == 1:
+        return s + "-S"
+    else:
+        bies = [s[0] + "-B"]
+        for i in s[1:-1]:
+            bies.append(i + "-I")
+        bies.append(s[-1] + "-E")
+        return " ".join(bies)
+
+
+def segment_with_preprocessing(seg: Segmenter, sent:str) -> str:
+    tokens = []
+    for group in tokenize_by_unicode_category(sent):
+        try:
+            if ud.name(group[0]).startswith("CJK"):
+                tokens.append(seg.segmentSentenceBIES(" ".join(group)))
+            else:
+                tokens.append(add_bies(group))
+        except:
+            tokens.append(add_bies(group))
+    return " ".join(tokens)
