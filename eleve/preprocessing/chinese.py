c5130b5d3322e67d15bfbe2378c58985c6a2b8c2
Pierre M <pierre.magistry@univ-amu.fr>
2020-09-08 20:25:17 +0200

    preproc for ner paper

--- eleve/preprocessing/chinese.py
+++ eleve/preprocessing/chinese.py
@@ -31,25 +31,27 @@ def filter_cjk(toks: List[str]) -> List[str]:
         except:
             pass
 
-def add_bies(s: str) -> str:
+def add_bies(s: str) -> List[str]:
     if len(s) == 1:
-        return s + "-S"
+        return [s + "-S"]
     else:
         bies = [s[0] + "-B"]
         for i in s[1:-1]:
             bies.append(i + "-I")
         bies.append(s[-1] + "-E")
-        return " ".join(bies)
-
+        return bies
 
 def segment_with_preprocessing(seg: Segmenter, sent:str) -> str:
     tokens = []
     for group in tokenize_by_unicode_category(sent):
         try:
             if ud.name(group[0]).startswith("CJK"):
-                tokens.append(seg.segmentSentenceBIES(" ".join(group)))
+                words = seg.segment(group)
+                for w in words:
+                    tokens.extend(add_bies(w))
             else:
-                tokens.append(add_bies(group))
+                tokens.extend(add_bies(group))
         except:
-            tokens.append(add_bies(group))
-    return " ".join(tokens)
+            tokens.extend(add_bies(group))
+    return "\n".join(tokens)
+
